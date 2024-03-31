8389e5f88ecca3a98e747fdb35ae0b894d3ff9f0
Harsh Tejani <tejani317@gmail.com>
2024-03-30 22:45:38 -0400

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { TextField } from "@mui/material";
 import "./App.css";
-import CardsPage from "./pages/CardsPage";
-import Login from "./pages/Login";
 
 function App() {
   return <>{/* <CardsPage /> */}</>;
